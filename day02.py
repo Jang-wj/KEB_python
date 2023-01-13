01f985fe5a4e0dca8f7b0e4945fc1a51d2dc1ee8
unknown <jwj5230@gmail.com>
2023-01-13 15:26:07 +0900

    day02 if random

--- day02.py
+++ day02.py
@@ -1,21 +1,7 @@
-# Chapter 4
-
-a = []
-print(bool(a))
-a.append(5)
-print(bool(a))
-print(bool(set()))
-print(bool(dict()))
-print(bool(''))
-print(bool('aaa'))
-
-letter = 'o'
-vowels = 'aeiou'
-if letter in vowels:
-    print('실행')
+import random
 
 limits = 20
-tweets = 'home' * 6
+tweets = 'home' * random.randint(1, 10)
 diff = limits - len(tweets)
 # if diff := limits - len(tweets) >= 0 :
 if diff >= 0 :
