a26eda8f4043aa0a5d1de697f5abba28e70691c2
JangWooJeong <jwj5230@gmail.com>
2023-01-16 14:35:01 +0900

    day03 prime number2

--- day03.py
+++ day03.py
@@ -2,22 +2,12 @@
 
 number = int(input('number :'))
 counts = 0
-# k =1
-# while k <= number:
-#     if number % k == 0:
-#         counts += 1
-#     k += 1
-#
-# if counts == 2:
-#     print(f'{number} is prime number')
-# else:
-#     print(f'{number} is not prime number')
 
-for i in range(1, number + 1):
+for i in range(2, number):
     if number % i == 0:
         counts += 1
 
-if counts == 2:
-    print(f'{number} is prime number')
-else:
+if counts:
     print(f'{number} is not prime number')
+else:
+    print(f'{number} is prime number')
