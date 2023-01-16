936a44b9d1ce24e21e5f16b68c94a6ddd9183fee
JangWooJeong <jwj5230@gmail.com>
2023-01-16 14:43:45 +0900

    day03 prime number4 bool break

--- day03.py
+++ day03.py
@@ -1,11 +1,14 @@
 # prime number
 
-number = int(input('number :'))
+number = int(input('number : '))
 is_prime = True
 
 for i in range(2, number):
     if number % i == 0:
         is_prime = False
+        break
+    print(i)
+
 
 if is_prime:
     print(f'{number} is prime number')
