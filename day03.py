5998b215d3b4d6bf5a69297cdc75bc1f441b29cd
JangWooJeong <jwj5230@gmail.com>
2023-01-16 14:18:58 +0900

    day03 loop

--- day03.py
+++ day03.py
@@ -1,38 +1,12 @@
-univ = 'Inha University'
-
-print(univ[5:])
-print(univ[5:15])  # end-1
-print(univ[-10:])
-print(univ[::2])
-print(univ[5:-6])
-print(univ[-10:-6])
-
-print(len(univ))
-print(univ.split())  # delimiter
-
-pokemons_list = ['피카츄', '꼬부기', '파이리', '이상해']
-pokemons_string = ' / '.join(pokemons_list)
-print(pokemons_string)
-
-inha = 'a duck goes into a bar'
-print(inha.replace('a ', 'a nice '))
-
-subject = ' $ python, data structure, database $$$'
-print(subject.strip('$'))
-
-print(subject.find('data'), subject.index('data'))
-print(subject.find('inha'))  # -1 return
-# print(subject.index('inha'))   # exception
-print(subject.rfind('data'), subject.rindex('data'))
-print(subject.count('data'))
-
-# 5-1 연습문제
-song = '''When an eel grabs your arm, And it causes great harm, That's a moray'''
-song_list = song.split()
-if 'moray' in song_list:
-    song_list[-1] = song_list[-1].title()
-    song_string = ' '.join(song_list)
-    print(song_string)
-
-
+# while
+while True:
+    dan = int(input('Dan? (0 to quit)'))
+    if dan == 0:
+        break
+    if 1 < dan < 10:
+        for i in range(1, 10):
+            print(f'{dan} * {i} = {dan * i}')
+            i = i + 1
+    else:
+        print('2~9 사이 숫자 입력')
 
