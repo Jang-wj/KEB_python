79453c7fddf2690de5ae54a3ddf2fc68358e5ede
JangWooJeong <jwj5230@gmail.com>
2023-01-17 17:49:14 +0900

    day04 mission

--- day04.py
+++ day04.py
@@ -1,71 +1,131 @@
-# # Dictionary and Set
-#
-# students = {'name': 'kim inha', 'age': 23, 'eyes': [0.9, 1.1]}
-# # for k in students: = for k in students.keys():
-# # for k in students.values():
-# for k, v in students.items():
-#     print(f'{k} : {v}')
-# print(f'이름은 {students["name"]}, 나이는 {students["age"]}세', end=' ')
-# print(f'시력은 좌: {students["eyes"][0]}, 우: {students["eyes"][1]}')
+# 7-1
+
+year_list = [x for x in range(1999, 2005)]
+print(year_list)
+
+# 7-2
+print('----------------------------')
+
+print(year_list[3])
+
+# 7-3
+print('----------------------------')
+
+print(year_list[-1])
+
+# 7-8
+
+surprise = ['Groucho', 'Chico', 'Harpo']
+
+# 7-9
+print('----------------------------')
+
+surprise[-1] = surprise[-1].lower()
+print(surprise[-1][::-1].title())
+
+# 7-10
+
+num_list = [x for x in range(10) if x % 2 == 0]
+print(num_list)
+
+# 7-11
+print('----------------------------')
 
 
-# alcohol_foods = {
-#     '맥주': '치킨', '소주': '골뱅이 소면', '위스키': '치즈', '고량주': '짬뽕'
-# }
-# alcohol_list = list(alcohol_foods)  # 키 값만 추출
-# while True:
-#     alcohol = input(f'술을 선택하세요 1. {alcohol_list[0]} 2. {alcohol_list[1]} 3. {alcohol_list[2]} 4. {alcohol_list[3]} 5. 계산:')
-#     if alcohol == '5':
-#         print('다음에 또 오세요!')
-#         break
-#     elif alcohol == '1':
-#         print(f'{alcohol_list[0]}에 어울리는 안주는 {alcohol_foods[alcohol_list[0]]}입니다.')
-#     elif alcohol == '2':
-#         print(f'{alcohol_list[1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[1]]}입니다.')
-#     elif alcohol == '3':
-#         print(f'{alcohol_list[2]}에 어울리는 안주는 {alcohol_foods[alcohol_list[2]]}입니다.')
-#     elif alcohol == '4':
-#         print(f'{alcohol_list[3]}에 어울리는 안주는 {alcohol_foods[alcohol_list[3]]}입니다.')
-#     else:
-#         print('메뉴에서 골라주세요')
+start1 = ['fee', 'fie', 'foe']
+rhymes = [('flop', 'get a mop'),
+          ('fope', 'turn the rope'),
+          ('fa', 'get the cat'),
+          ('fudge', 'call the judge'),
+          ('fat', 'pet the cat'),
+          ('fog', 'walk the dog'),
+          ('fun', 'say were done')
+          ]
+start2 = 'Someone better'
 
-import random
-alcohol_foods = {
-    '맥주': '치킨', '소주': '골뱅이 소면', '위스키': '치즈', '고량주': '짬뽕'
+for i in start1:
+    print(f'{i.title()}!', end=' ')
+print(f'{rhymes[0][0].title()}!')
+
+for i, j in rhymes:
+    print(f'{start2} {j}.')
+
+
+# 8-1
+print('----------------------------')
+e2f = {
+    'dog': 'chien',
+    'cat': 'chat',
+    'walrus': 'morse'
 }
-alcohol_list = list(alcohol_foods)  # 키 값만 추출
-food_list = [food for food in alcohol_foods.values()]  # value 값 추출
-# while True:
-#     alcohol = input(f'술을 선택하세요 1. {alcohol_list[0]} 2. {alcohol_list[1]} 3. {alcohol_list[2]} 4. {alcohol_list[3]} 5. 아무거나: 6. 계산')
-#     if alcohol == '6':
-#         print('다음에 또 오세요!')
-#         break
-#     elif alcohol == '1':
-#         print(f'{alcohol_list[0]}에 어울리는 안주는 {alcohol_foods[alcohol_list[0]]}입니다.')
-#     elif alcohol == '2':
-#         print(f'{alcohol_list[1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[1]]}입니다.')
-#     elif alcohol == '3':
-#         print(f'{alcohol_list[2]}에 어울리는 안주는 {alcohol_foods[alcohol_list[2]]}입니다.')
-#     elif alcohol == '4':
-#         print(f'{alcohol_list[3]}에 어울리는 안주는 {alcohol_foods[alcohol_list[3]]}입니다.')
-#     elif alcohol == '5':
-#         print(f'{alcohol_list[random.randint(0, 3)]}에 어울리는 안주는 {food_list[random.randint(0, 3)]}입니다.')
-#     else:
-#         print('메뉴에서 골라주세요')
+print(e2f)
 
+# 8-2
+print('----------------------------')
+print(e2f['walrus'])
 
-# for i in range(len(food_list)):
-#     print(food_list[i])
-# for food in food_list:   # more pythonic code
-#     print(food)
-# for food in enumerate(food_list):  # tuple 형태로 인덱스와 함께 return
-#     print(food)
+# 8-3
+print('----------------------------')
 
+# f2e = {}
+# for i, j in e2f.items():
+#     print(i)
+#     f2e[j] = i
+# print(f2e)
 
-# first = {'a': 'agony', 'b': 'bliss'}
-# second = {'b': 'bagels', 'c': 'candy'}
-# print({**first, **second})
-# third = {'d': 'donuts'}
-# print({**first, **second, **third})
+f2e = {j: i for i, j in e2f.items()}
+print(f2e)
 
+# 8-4
+print('----------------------------')
 
+print([x for x, y in e2f.items() if y == "chien"])
+
+# 8-5
+print('----------------------------')
+
+print(e2f.keys())
+
+# 8-6, 8-7, 8-8, 8-9
+print('----------------------------')
+
+life = {
+    'animals': {'cats': 'Henri', 'octopi': 'Grumpy', 'emus': 'Lucy'},
+    'plants': '',
+    'other': ''
+}
+print(life.keys())
+print(life['animals'].keys())
+print(life['animals']['cats'])
+
+# 8-10
+print('----------------------------')
+
+squares = {x: x**2 for x in range(10)}
+print(squares)
+
+# 8-12
+print('----------------------------')
+
+print(x for x in 'Got')
+for x in 'Got':
+    print(x)
+print(x for x in range(10))
+for x in range(10):
+    print(x)
+
+# 8-13
+print('----------------------------')
+
+tup1 = ('optimist', 'pessimist', 'troll')
+tup2 = ('The glass is half full', 'the glass is half empty', 'How did you get a glass?')
+
+dic = {x: y for x, y in zip(tup1, tup2)}
+print(dic)
+
+# 8-14
+print('----------------------------')
+title = ['Creature of Habit', 'Crewel Fate']
+plot = ['A nun turns into a monster', 'A haunted yarn shop']
+movies = {x: y for x, y in zip(title, plot)}
+print(movies)
