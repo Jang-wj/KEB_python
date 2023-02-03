8be1beed314bcf6d878743f776dd66adf65b900a
JangWooJeong <jwj5230@gmail.com>
2023-02-03 16:41:20 +0900

    day15 ㅡmission

--- day15.py
+++ day15.py
@@ -1,65 +1,39 @@
-def printPoly(tx, px):
-    """
-    다항식을 포맷에 맞게 출력
-    :param tx: 차수를 원소로 가지고 있는 list
-    :param px: 계수를 원소로 가지고 있는 list
-    :return: 다항식 문자열
-    """
-    poly_str = "P(x) = "
+def find_and_insert_data(friend, k_count):  # idx
+    findPos = -1
+    for i in range(len(katok[1])):
+        pair = katok[1][i]
+        if k_count >= pair:
+            findPos = i
+            break
+    if findPos == -1:
+        findPos = len(katok[1])
 
-    for i in range(len(px)):
-        term = tx[i]  # 차수
-        coef = px[i]  # 계수
-
-        if coef > 0 and i > 0:
-            poly_str += "+"
-        elif coef < 0:
-            poly_str += ""
-        elif coef == 0:
-            term -= 1
-            continue
-
-        poly_str += f'{coef}x^{term} '
-        term -= 1
-
-    return poly_str
+    insert_data(findPos, friend, k_count)
 
 
-def calcPoly(tx, px):
-    """
-    다항식을 계산
-    :param tx: 차수를 원소로 가지고 있는 list
-    :param px: 계수를 원소로 가지고 있는 list
-    :return: 다항식 계산 결과 값 integer
-    """
-    ret_value = 0
+def insert_data(position, friend, k_count):
+    if position < 0 or position > len(katok[1]):
+        print("데이터를 삽입할 범위를 벗어났습니다.")
+        return
 
-    for i in range(len(px)):
-        term = tx[i]
-        coef = px[i]  # 계수
-        ret_value += coef * x_value ** term
-        term -= 1
+    for i in range(len(katok)):
+        katok[i].append(None)  # 빈 칸 추가
 
-    return ret_value
+    for i in range(len(katok)):
+        for k in range(len(katok[1]) - 1, position, -1):
+            katok[i][k] = katok[i][k - 1]
+            katok[i][k - 1] = None
+
+    katok[0][position] = friend
+    katok[1][position] = k_count
 
 
-tx = [300, 20, 0]
-px = [7, -4, 5]
-
+# katok = [('다현', 200), ('정연', 150), ('쯔위', 90), ('사나', 30), ('지효', 15)]
+katok = [['다현', '정연', '쯔위', '사나', '지효'], [200, 150, 90, 30, 15]]
 
 if __name__ == "__main__":
-    t = int(input("차수 입력"))
-    p = int(input("계수 입력"))
-    tx.append(t)
-    tx.sort(reverse=True)
-    px.insert(tx.index(t), p)
-
-    pStr = printPoly(tx, px)
-    print(pStr)
-
-    x_value = int(input("X 값-->"))
-
-    px_value = calcPoly(tx, px)
-    print(px_value)
-
-
+    # while True:
+        data = input("추가할 친구--> ")
+        count = int(input("카톡 횟수--> "))
+        find_and_insert_data(data, count)
+        print(katok)
