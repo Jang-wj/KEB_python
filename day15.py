08c4833cd36dff2214851695c031ecb28ebddeff
JangWooJeong <jwj5230@gmail.com>
2023-02-03 14:13:48 +0900

    day15 linear list 03-3

--- day15.py
+++ day15.py
@@ -1,73 +1,57 @@
-def insert_data(idx, pokemon):
+def printPoly(px):
     """
-    선형리스트에 값을 중간에 삽입
-    :param idx:
-    :param pokemon:
-    :return:
+    다항식을 포맷에 맞게 출력
+    :param px: 계수를 원소로 가지고 있는 list
+    :return: 다항식 문자열
     """
-    if idx < 0 or idx > len(pokemons):
-        print("데이터를 삽입할 범위를 벗어났습니다.")
-        return
+    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+    poly_str = "P(x) = "
 
-    pokemons.append(None)  # 빈칸 추가
+    for i in range(len(px)):
+        coef = px[i]  # 계수
 
-    for i in range(len(pokemons)-1, idx, -1):
-        pokemons[i] = pokemons[i - 1]
-        pokemons[i - 1] = None
+        if coef > 0 and i > 0:
+            poly_str += "+"
+        elif coef < 0:
+            poly_str += ""
+        elif coef == 0:
+            term -= 1
+            continue
 
-    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
+        poly_str += f'{coef}x^{term} '
+        term -= 1
 
-def delete_data(idx):
+    return poly_str
+
+
+def calcPoly(x_val, px):
     """
-    선형 리스트에 idx값 뒤의 값들을 제거
-    :param idx:
-    :return:
+    다항식을 계산
+    :param x_val: x 값 integer
+    :param px: 계수를 원소로 가지고 있는 list
+    :return: 다항식 계산 결과 값 integer
     """
-    if idx < 0 or idx > len(pokemons):
-        print("데이터를 삭제할 범위를 벗어났습니다.")
-        return
+    ret_value = 0
+    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
 
-    for _ in range(len(pokemons) - idx):
-        pokemons.pop()
-    # for i in range(idx + 1, len(pokemons)):
-    #     pokemons[i] = None
-    #
-    # for i in range(idx, len(pokemons)):
-    #     pokemons.pop()
+    for i in range(len(px)):
+        coef = px[i]  # 계수
+        ret_value += coef * xValue ** term
+        term -= 1
 
-    # del pokemons[idx:]
+    return ret_value
 
-def add_data(pokemon):
-    """
-    선형 리스트에 pokemon 추가
-    :param pokemon:
-    :return:
-    """
-    pokemons.append(None)
-    pokemons[len(pokemons)-1] = pokemon
 
-pokemons = []
+px = [3, -4, 0, 6]  # = 3x^3 -4x^2 +0x^1 +6x^0
+
 
 if __name__ == "__main__":
-    while True:
-        menu = int(input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> "))
-        if (menu == 1):
-            data = input("추가할 데이터--> ")
-            add_data(data)
-            print(pokemons)
-        elif (menu == 2):
-            pos = int(input("삽입할 위치--> "))
-            data = input("추가할 데이터--> ")
-            insert_data(pos, data)
-            print(pokemons)
-        elif (menu == 3):
-            pos = int(input("삭제할 위치--> "))
-            delete_data(pos)
-            print(pokemons)
-        elif (menu == 4):
-            print(pokemons)
-            # exit()
-            break
-        else:
-            print("1~4 중 하나를 입력하세요.")
-            continue
+    pStr = printPoly(px)
+    print(pStr)
+
+    xValue = int(input("X 값-->"))
+
+    pxValue = calcPoly(xValue, px)
+    print(pxValue)
+
+
