a0e0bd92dd68b653fa8ad30f0276986d801cc760
JangWooJeong <jwj5230@gmail.com>
2023-02-06 17:43:06 +0900

    day16 mission ex0401

--- day16.py
+++ day16.py
@@ -1,30 +1,60 @@
-"""
-연습문제
-4-1
- 1. 선형 리스트
- 2. 연결 리스트
+# 응용예제 4-1
 
-4-2
- 답: 3
+class Node():
+    def __init__(self):
+        self.data = None
+        self.link = None
 
-4-3
-  a = 데이터
-  b = 링크
 
-4-4
- 답: 2
+def print_nodes(start):
+    current = start
+    print(current.data, end=' ')
+    while True:
+        if current.link == None:
+            break
+        current = current.link
+        print(current.data, end=' ')
+    print()
 
-4-5
- 답: b - a - c
 
-4-6
- 답: 4
+def make_linked_list(data):
+    global linked_list, current, head, pre
 
-4-7
- 1. node.data
- 2. node.link
- 3. node
+    node = Node()
+    node.data = data
+    linked_list.append(node)
+    if head == None:
+        head = node
+        return
 
-4-8
- 답: 2
-"""
+    if head.data[1] > data[1]:
+        node.link = head
+        head = node
+        return
+
+    current = head
+    while True:
+        if current.link == None:
+            break
+        pre = current
+        current = current.link
+        if current.data[1] > data[1]:
+            pre.link = node
+            node.link = current
+            return
+
+    current.link = node
+
+
+linked_list = []
+head, current, pre = None, None, None
+
+if __name__ == "__main__":
+    while True:
+        name = input("name :")
+        if name == '' or name == None:
+            break
+        email = input("email :")
+        make_linked_list([name, email])
+        print_nodes(head)
+
