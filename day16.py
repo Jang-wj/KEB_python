462f63c9e4de8a2a9460209083000751639ec85b
JangWooJeong <jwj5230@gmail.com>
2023-02-06 18:29:39 +0900

    day16 mission Q5

--- day16.py
+++ day16.py
@@ -1,81 +1,21 @@
-# 응용예제 4-2
+"""
+연습문제
+5-1, 3,4
 
-import random
+5-2, node1.link = node1
 
-class Node():
-    def __init__(self):
-        self.data = None
-        self.link = None
+5-3, node2.link = node3.link
+     del(node3)
 
+5-4, 2
 
-def print_nodes(start):
-    current = start
-    print(current.data, end=' ')
-    while True:
-        if current.link == None:
-            break
-        current = current.link
-        print(current.data, end=' ')
-    print()
+5-5
+ 1. current = head
+ 2. last = last.link
+ 3. last.link = head
 
-
-def make_lotto_list(num):
-    global linked_list, current, head, pre
-
-    node = Node()
-    node.data = num
-    linked_list.append(node)
-    if head == None:
-        head = node
-        return
-
-    if head.data > num:
-        node.link = head
-        head = node
-        return
-
-    current = head
-    while True:
-        if current.link == None:
-            break
-        pre = current
-        current = current.link
-        if current.data > num:
-            pre.link = node
-            node.link = current
-            return
-
-    current.link = node
-
-def find_lotto(num):
-    global linked_list, head, current, pre
-
-    if head == None:
-        return False
-    current = head
-    if current.data == num:
-        return True
-    while True:
-        if current.link == None:
-            break
-        current = current.link
-        if current.data == num:
-            return True
-    return False
-
-
-
-linked_list = []
-head, current, pre = None, None, None
-
-if __name__ == "__main__":
-    cnt = 0
-    while cnt <= 6:
-        num = random.randint(1, 45)
-        if find_lotto(num):
-            continue
-        make_lotto_list(num)
-        cnt += 1
-
-    print_nodes(head)
-
+5-6
+ 1. return current
+ 2. current.link != head
+ 3. current = current.link
+"""
