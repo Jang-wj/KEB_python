e724d5e0cc16281784fbf3240591cc89f95320e1
JangWooJeong <jwj5230@gmail.com>
2023-02-06 18:45:22 +0900

    day16 mission ex0501

--- day16.py
+++ day16.py
@@ -1,21 +1,83 @@
-"""
-연습문제
-5-1, 3,4
+import random
+import math
 
-5-2, node1.link = node1
 
-5-3, node2.link = node3.link
-     del(node3)
+class Node():
+    def __init__(self):
+        self.data = None
+        self.link = None
 
-5-4, 2
 
-5-5
- 1. current = head
- 2. last = last.link
- 3. last.link = head
+def print_stores(start):
+    current = start
+    if current == None:
+        return
 
-5-6
- 1. return current
- 2. current.link != head
- 3. current = current.link
-"""
+    while True:
+        if current.link == head:
+            break
+        current = current.link
+        x, y = current.data[1:]
+        print(current.data[0], '편의점, 거리:', math.sqrt(x * x + y * y))
+    print()
+
+
+def make_store_list(store):
+    global c_linked_list, head, current, pre
+
+    node = Node()
+    node.data = store
+    c_linked_list.append(node)
+
+    if head == None:
+        head = node
+        node.link = head
+        return
+
+    nodeX, nodeY = node.data[1:]
+    nodeDist = math.sqrt(nodeX ** 2 + nodeY ** 2)
+    headX, headY = head.data[1:]
+    headDist = math.sqrt(nodeX ** 2 + nodeY ** 2)
+
+    if headDist > nodeDist:
+        node.link = head
+        last = head
+        while last.link != head:
+            last = last.link
+        last.link = node
+        head = node
+        return
+
+    current = head
+    while True:
+        if current.link == head:
+            break
+        pre = current
+        current = current.link
+        currX, currY = current.data[1:]
+        currDist = math.sqrt(currX ** 2 + currY ** 2)
+        if currDist > nodeDist:
+            pre.link = node
+            node.link = current
+            return
+
+    current.link = node
+    node.link = head
+
+
+c_linked_list = []
+head, current, pre = None, None, None
+
+if __name__ == "__main__":
+
+    store_array = []
+    store_name = 'A'
+    for _ in range(10):
+        store = (store_name, random.randint(1, 100), random.randint(1, 100))
+        store_array.append(store)
+        store_name = chr(ord(store_name) + 1)
+
+    for store in store_array:
+        make_store_list(store)
+
+    print_stores(head)
